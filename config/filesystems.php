fe2f4f947a7f2149930dd59af5e4d3afede04395
zouzhi <zouzhi@chinasofti.com>
2023-07-05 03:09:30 +0800

    f

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DRIVER', 'oss'),
 
     /*
     |--------------------------------------------------------------------------
@@ -36,28 +36,20 @@ return [
             'throw' => false,
         ],
 
-        // 'uploads' => [
-        //     'driver' => 'local',
-        //     'root' => storage_path('app/uploads'),
-        //     'url' => '/storage/app/uploads',
-        //     'visibility' => 'public',
-        //     'throw' => false,
-        // ],
+        'uploads' => [
+            'driver' => 'oss',
+            'root' => storage_path('app/uploads'),
+            'url' => '/storage/app/uploads',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
 
         'media' => [
-            "driver"            => "oss",
-            "access_key_id"     => env('AWS_ACCESS_KEY_ID'),        // 必填, 阿里云的AccessKeyId
-            "access_key_secret" => env('ALIYUN_SECRET_ACCESS_KEY'),      // 必填, 阿里云的AccessKeySecret
-            "bucket"            => env('ALIYUN_BUCKET'),               // 必填, 对象存储的Bucket, 示例: my-bucket
-            "endpoint"          => env('ALIYUN_ENDPOINT'),        // 必填, 对象存储的Endpoint, 示例: oss-cn-shanghai.aliyuncs.com
-            "internal"          => env("OSS_INTERNAL", null),          // 选填, 内网上传地址,填写即启用 示例: oss-cn-shanghai-internal.aliyuncs.com
-            "domain"            => null,            // 选填, 绑定域名,填写即启用 示例: oss.my-domain.com
-            "prefix"            => "storage/app/media",              // 选填, 统一存储地址前缀
-            "use_ssl"           => false,              // 选填, 是否使用HTTPS
-            "reverse_proxy"     => env("OSS_REVERSE_PROXY", false),    // 选填, 域名是否使用NGINX代理绑定
-            "throw"             => env("OSS_THROW", false),            // 选填, 是否抛出引起错误的异常,默认出现错误时,不抛出异常仅返回false
-            "options"           => [],                                 // 选填, 添加全局配置参数, 示例: [\OSS\OssClient::OSS_CHECK_MD5 => false]
-            "macros"            => [],
+            'driver' => 'oss',
+            'root' => storage_path('app/media'),
+            'url' => '/storage/app/media',
+            'visibility' => 'public',
+            'throw' => false,
         ],
 
         'resources' => [
@@ -80,7 +72,7 @@ return [
             'throw' => false,
         ],
 
-        'uploads' => [
+        'oss' => [
             "driver"            => "oss",
             "access_key_id"     => env('AWS_ACCESS_KEY_ID'),        // 必填, 阿里云的AccessKeyId
             "access_key_secret" => env('ALIYUN_SECRET_ACCESS_KEY'),      // 必填, 阿里云的AccessKeySecret
